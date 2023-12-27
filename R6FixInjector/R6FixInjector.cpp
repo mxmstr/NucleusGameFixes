d9ebb5e327c8a2ce716a29154c7b3ef16e18d61b
mxmstr <lynch.er18@gmail.com>
2023-12-26 19:45:15 -0500

    Injector

--- R6FixInjector/R6FixInjector.cpp
+++ R6FixInjector/R6FixInjector.cpp
@@ -32,45 +32,9 @@ int _tmain(int argc, _TCHAR* argv[])
     si.cb = sizeof(STARTUPINFO);
     ZeroMemory(&pi, sizeof(pi));
 
-    if (argc <= 1)
-    {
-        printf(PROGNAME " - Launches other applications and hooks function calls\n");
-        printf("            to allow for the export of non-exportable certificates.\n");
-        printf("-------------------------------------------------------------------\n");
-        printf("Usage:\n");
-        printf(" " PROGNAME " <program> <options>\n\n");
-        return -1;
-    }
-
     // See if there is a log file which indicates we should log our progress.
     hLogFile = CreateFile("r6fix.log", GENERIC_WRITE, FILE_SHARE_WRITE | FILE_SHARE_READ,
                           NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
-
-    // Using argv[] and GetCommandLine so I can steal the correctly quoted command line from GetCommandLine
-    // instead of using argv[] and concatenating a bunch of strings and getting quoting correct. Using
-    // argv[] so I know where the arguments start in the string from GetCommandLine
-    pFirstArg = argv[1];
-    pPos = _tcsstr(lptstr, pFirstArg);
-    if (pPos == NULL)
-    {
-        printf("Could not find first argument [%S] in command line [%S]\n", pFirstArg, lptstr);
-        return -1;
-    }
-
-    // If first parameter is quoted the quote doesn't show up in argv[] but
-    // it does show up in GetCommandLine.
-    if (pPos[-1] == _T('\"') || pPos[-1] == _T('\''))
-        pPos--;
-
-    len = _tcslen(pPos);
-    pCommandLine = (TCHAR*)calloc(len + 1, sizeof(TCHAR));
-    if (!pCommandLine)
-    {
-        printf("Failed to allocate memory\n");
-        return -1;
-    }
-
-    StringCchCopy(pCommandLine, len + 1, pPos);
 
     if (hLogFile != INVALID_HANDLE_VALUE)
     {
@@ -84,6 +48,8 @@ int _tmain(int argc, _TCHAR* argv[])
             hLogFile = INVALID_HANDLE_VALUE;
         }
     }
+
+    pCommandLine = "RainbowSix.exe";
 
     if (hLogFile != INVALID_HANDLE_VALUE)
     {
